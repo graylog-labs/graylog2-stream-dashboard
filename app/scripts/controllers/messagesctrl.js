b792799e5414ce6e8da69bbf6e07cb878004e121
Dennis Oelkers <dennis@lauschmusik.de>
2014-01-27 13:18:10 +0100

    First working sample

--- app/scripts/controllers/messagesctrl.js
+++ app/scripts/controllers/messagesctrl.js
@@ -1,9 +1,19 @@
 'use strict';
 
 angular.module('graylog2StreamdashApp')
-  .controller('MessagesCtrl', ['$scope', '$routeParams', 'Message', function ($scope, $routeParams, Message) {
+  .controller('MessagesCtrl', ['$scope', '$routeParams', 'Message', 'Stream', function ($scope, $routeParams, Message, Stream) {
 	console.log($routeParams);
+	Stream.get({id: $routeParams.id}, function(response) {
+		$scope.stream = response;
+	});
+	
 	Message.get({ streamId: $routeParams.id}, function(response) {
 		$scope.messages = response.messages.map(function(x) { return x.message; });
 	});
+	
+	setInterval(function() {
+		Message.get({ streamId: $routeParams.id}, function(response) {
+			$scope.messages = response.messages.map(function(x) { return x.message; });
+		});
+	}, 2000);
 }]);
--- app/scripts/services/streams_services.js
+++ app/scripts/services/streams_services.js
@@ -5,7 +5,7 @@ var streamsServices = angular.module('streamsServices', ['ngResource']);
 streamsServices.factory('Stream', ['$resource',
 	function($resource) {
 		return $resource('http://localhost:12900/streams/:id', {id: '@id'}, {
-			query: { method: 'GET' }
+			query: { method: 'GET', params: {id: ''} }
 		});
 	}
 ]);
