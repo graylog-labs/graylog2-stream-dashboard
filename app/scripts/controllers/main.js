c1aadb8f70159b2c027699198a8703a4097e1725
Dennis Oelkers <dennis@lauschmusik.de>
2014-01-27 11:46:01 +0100

    Initial checkin

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('graylog2StreamdashApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
