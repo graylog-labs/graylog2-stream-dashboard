9316540abbbfea146c3d12c613e33a042711dc3f
Dennis Oelkers <dennis@lauschmusik.de>
2014-01-28 10:07:28 +0100

    Starting updater thread only one, not updating when id is undefined

--- app/scripts/controllers/messagesctrl.js
+++ app/scripts/controllers/messagesctrl.js
@@ -9,13 +9,19 @@ angular.module('graylog2StreamdashApp')
 	Message.get({ streamId: $routeParams.id}, function(response) {
 		$scope.messages = response.messages.map(function(x) { return x.message; });
 	});
-	
-	setInterval(function() {
-		Message.get({ streamId: $routeParams.id}, function(response) {
-			$scope.messages = response.messages.map(function(x) { return x.message; });
-		});
-		StreamAlert.query({id: $routeParams.id}, function(response) {
-			$scope.streamAlerts = response.alerts;
-		});
-	}, 2000);
+
+	if (!$scope.updaterThread) {
+		$scope.updaterThread = setInterval(function() {
+			console.log("updating data: " + $routeParams.id);
+			if ($routeParams.id == undefined)
+				return;
+			Message.get({ streamId: $routeParams.id}, function(response) {
+				$scope.messages = response.messages.map(function(x) { return x.message; });
+			});
+			StreamAlert.query({id: $routeParams.id}, function(response) {
+				$scope.streamAlerts = response.alerts;
+			});
+		}, 2000);
+	}
+
 }]);
