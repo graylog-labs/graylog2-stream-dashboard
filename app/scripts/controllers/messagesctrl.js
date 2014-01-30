af4e336443b8450793d461235cf1ddb72a538171
Dennis Oelkers <dennis@lauschmusik.de>
2014-01-30 12:18:41 +0100

    Refactoring a bit

--- app/scripts/controllers/messagesctrl.js
+++ app/scripts/controllers/messagesctrl.js
@@ -2,33 +2,30 @@
 
 angular.module('graylog2StreamdashApp')
   .controller('MessagesCtrl', ['$scope', '$routeParams', 'Message', 'Stream', 'StreamAlert', function ($scope, $routeParams, Message, Stream, StreamAlert) {
-	Stream.get({id: $routeParams.id}, function(response) {
-		$scope.stream = response;
-	});
-	
-	Message.get({ streamId: $routeParams.id}, function(response) {
-		for (var field in response.messages[0].message) {
-			console.log(field);
-		}
-		$scope.messages = response.messages.map(function(x) { return x.message; });
-		var fields = response.fields;
-		fields.shift('message');
-		$scope.fields = fields.sort();
-	});
+  	$scope.update = function() {
+		Stream.get({id: $routeParams.id}, function(response) {
+			$scope.stream = response;
+		});
+		
+		Message.get({ streamId: $routeParams.id}, function(response) {
+			$scope.messages = response.messages.map(function(x) { return x.message; });
+			var fields = response.fields;
+			fields.shift('message');
+			$scope.fields = fields.sort();
+		});
+		StreamAlert.query({id: $routeParams.id}, function(response) {
+			$scope.streamAlerts = response.alerts;
+			$scope.totalStreamAlerts = response.total;
+		});
+  	};
 
 	if (!$scope.updaterThread) {
 		$scope.updaterThread = setInterval(function() {
 			if ($routeParams.id == undefined)
 				return;
-			Message.get({ streamId: $routeParams.id}, function(response) {
-				$scope.lastUpdate = new Date();
-				$scope.messages = response.messages.map(function(x) { return x.message; });
-			});
-			StreamAlert.query({id: $routeParams.id}, function(response) {
-				$scope.streamAlerts = response.alerts;
-				$scope.totalStreamAlerts = response.total;
-			});
+			$scope.update();
 		}, 2000);
 	}
 
+	$scope.update();
 }]);
--- app/scripts/controllers/settingsctrl.js
+++ app/scripts/controllers/settingsctrl.js
@@ -3,7 +3,6 @@
 angular.module('graylog2StreamdashApp')
   .controller('SettingsCtrl', ['$scope', '$routeParams', 'settings', '$window', function ($scope, $routeParams, Settings, $window) {
 	$scope.update = function() {
-		console.log(Settings);
 		Settings.set($scope.settings);
 		$window.location.href = '#/';
 		$window.location.reload();
