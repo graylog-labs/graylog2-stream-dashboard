fd92155dc6ba810b964f1e557d3395e72ce12f32
Lennart Koopmann <lennart@socketfeed.com>
2014-02-15 19:05:12 +0100

    slice trailing slash off server URI to be easier to configure

--- app/scripts/controllers/settingsctrl.js
+++ app/scripts/controllers/settingsctrl.js
@@ -3,9 +3,15 @@
 angular.module('graylog2StreamdashApp')
   .controller('SettingsCtrl', ['$scope', '$routeParams', 'settings', '$window', function ($scope, $routeParams, Settings, $window) {
 	$scope.update = function() {
+		// Remove trailing slash if there is one.
+		if ($scope.settings.serverUrl.slice(-1) == "/") {
+			$scope.settings.serverUrl = $scope.settings.serverUrl.substring(0, $scope.settings.serverUrl.length - 1);	
+		}
+
 		Settings.set($scope.settings);
 		$window.location.href = '#/';
 		$window.location.reload();
 	};
+
 	$scope.settings = Settings;
 }]);
